1bef10b5eb9b3196af29bbfdb577fff0db9adf82
linn-dev <tenkotenko.me@gmail.com>
2025-10-07 11:43:15 +0630

    Fix error main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Add this
-import App from './App.jsx'; // Or your root
 import RegisterForm from './RegisterForm.jsx';
 import AttendanceForm from './AttendanceForm.jsx';
 import AdminDashboard from './AdminDashboard.jsx';
@@ -10,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter> {/* Wrap everything */}
             <Routes>
-                <Route path="/" element={"Hello, I am Yan Linn Aung(Linn Dev)"} /> {/* Home */}
+                <Route path="/" element={<div style={{textAlign: "center"}}><p>Hi!</p><h1>I am Yan Linn Aung</h1><p>Linn Dev</p></div>} /> {/* Home */}
                 <Route path="/register" element={<RegisterForm />} />
                 <Route path="/attendance" element={<AttendanceForm />} />
                 <Route path="/admin" element={<AdminDashboard />} />
