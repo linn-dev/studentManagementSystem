c1bac2bc5e61b2f2b5fa5f832af1d3ae58f5c2bd
linn-dev <tenkotenko.me@gmail.com>
2025-10-07 13:47:24 +0630

    Change some text on register form

--- src/RegisterForm.jsx
+++ src/RegisterForm.jsx
@@ -105,7 +105,7 @@ function RegisterForm() {
             return;
         }
         if (!/^[a-zA-Z0-9_]+$/.test(telegramUsername.trim())) {
-            setError('Telegram Username should only contain letters, numbers, or underscores.');
+            setError('Telegram Username should only contain letters, numbers, or underscores & don\'t need "@" Sign');
             return;
         }
         if (!generatedId) {
@@ -285,7 +285,7 @@ function RegisterForm() {
                             <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                             </svg>
-                            Download Card
+                            Download Your Student ID
                         </button>
                     </div>
                 </div>
