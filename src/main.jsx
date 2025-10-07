d5c2cdf092d3217b3cc7cf14a9b59cf730ff62bc
linn-dev <tenkotenko.me@gmail.com>
2025-10-07 11:33:33 +0630

    Update Route

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
-// main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import RegisterForm from './RegisterForm.jsx'
-import AttendanceFrom from './AttendanceForm.jsx';
-import AdminDashboard from './AdminDashboard';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Add this
+import App from './App.jsx'; // Or your root
+import RegisterForm from './RegisterForm.jsx';
+import AttendanceForm from './AttendanceForm.jsx';
+import AdminDashboard from './AdminDashboard.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/register" element={<RegisterForm />} />
-            <Route path="/attendanceForm" element={<AttendanceFrom />} />
-            <Route path="/admin" element={<AdminDashboard />} />
-        </Routes>
-    </BrowserRouter>
+    <React.StrictMode>
+        <BrowserRouter> {/* Wrap everything */}
+            <Routes>
+                <Route path="/" element={"Hello, I am Yan Linn Aung(Linn Dev)"} /> {/* Home */}
+                <Route path="/register" element={<RegisterForm />} />
+                <Route path="/attendance" element={<AttendanceForm />} />
+                <Route path="/admin" element={<AdminDashboard />} />
+                <Route path="*" element={<div>404 - Not Found</div>} /> {/* Catch-all */}
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
